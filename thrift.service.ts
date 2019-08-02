57784c9af511158eafdecb83a6a3f6bc52ee6312
lalalala <lalalala>
2019-08-02 17:17:59 +0300

    remove typings

--- thrift.service.ts
+++ thrift.service.ts
@@ -25,11 +25,11 @@ export class ThriftService {
     this.callback = callback;
   }
 
-  public call<T>(client, method: string, data?: Object, ...rest) {
-    return new Observable<T>((observer) => {
+  public call(client, method: string, data?: Object, ...rest) {
+    return new Observable<any>((observer) => {
       if(this.before_request) { this.before_request(this); }
 
-      const callback = (err: any, res: T) => {
+      const callback = (err: any, res) => {
         if(this.callback) { this.callback(err, res); }
         if (err) {
           observer.error(err);
